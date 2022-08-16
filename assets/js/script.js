0f99dc96fca7554c7723f31ecd0abcf09187d215
Your Name <you@example.com>
2022-08-16 13:31:20 -0400

    updated html structure

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var buttonEl = document.querySelector("#start-quiz");
+var responseItem = document.querySelector("#response-item");
+
+var startQuiz = function(){
+    buttonEl.addEventListener("click", function() {
+
+    })
+}
+
