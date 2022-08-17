3ae2db0d57bf771259423985ef125515a705fa75
Your Name <you@example.com>
2022-08-16 22:30:22 -0400

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,1 @@
-var buttonEl = document.querySelector("#start-quiz");
-var responseItem = document.querySelector("#response-item");
-
-var startQuiz = function(){
-    buttonEl.addEventListener("click", function() {
-
-    })
-}
 
