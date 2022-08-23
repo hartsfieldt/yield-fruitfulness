4175f436506548a1b9760b13dea536910f799f91
Your Name <you@example.com>
2022-08-22 20:38:08 -0400

    updated main page to transition to questions and updated timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,79 @@
-var timerEl = document.getElementById('timer');
-var questionHeader = document.getElementById('question-header');
-var timeLeft = 300000;
+var timerEl = document.getElementById("timer");
+var questionHeader = document.getElementById("question-header");
 var questionIndex = 0;
 
-var questions = [
+var quizButtons = document.querySelector(".answers");
+
+var startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", quizStarted);
+
+document.querySelector(".quiz").hidden = true;
+
+function quizStarted () {
+    document.querySelector(".main").hidden = true;
+    document.querySelector(".quiz").hidden = false;
+    countdown();
+    generateQuestion();
+}
+
+function generateQuestions () {
+    document.querySelector(".questions").textContent = questionArray[questionList].quizStarted
+    document.querySelector("#answer-one").textContent = questionArray[questionList].quizStarted
+    document.querySelector("#answer-two").textContent = questionArray[questionList].quizStarted
+    document.querySelector("#answer-three").textContent = questionArray[questionList].quizStarted
+    document.querySelector("#answer-four").textContent = questionArray[questionList].quizStarted
+}
+
+quizButton.addEventListener("click", nextQuestion);
+
+
+var quizList = [
     {
         question: "Javascript is an _______ language?",
-        choices: ["object-oriented", "objective-base", "procedural", "none of the above"],
+        choices: ["object-oriented", "objective-base", "\n", "procedural", "\n", "none of the above"],
         answer: "object-oriented"
     },
     {
-        question: "2. Which of the following keywords is used to define a variable in Javascript?",
+        question: "Which of the following keywords is used to define a variable in Javascript?",
         choices: ["var", "let", "Both A and B", "none of the above"],
         answer: "var"
     },
-
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "none of the above"],
+        answer: "Both A and B"
+    },
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["Throws an error", "Ignores the statements", "Gives a warning", "none of the above"],
+        answer: "Ignores the statements"
+    },
+    {   question: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: ["document.write()?", "console.log()", "window.alert()", "All of the above"],
+        answer: "All of the above"
+    }
 ]
 
 questionHeader.innerHTML = questions[0].question
 
-var quizStarted = function() {
-    questionHeader.innerHTML = questions[questionIndex].question;
-    for (let i = 0; i < questions[questionIndex].choices.length; i++) {
-        var btn = document.createElement("button");
-        btn.addEventListener("click", nextQuestion);
-        btn.innerHTML = questions[questionIndex].choices[i];
-        document.querySelector(".answers").append(btn)
-}
-}
-
 var nextQuestion = function() {
     questionIndex++;
-    quizStarted();
 }
 
-
-
 function countdown() {
-
-
-
-var timeInterval = setInterval(function () {
+    var timeLeft = 5;
+    var timeInterval = setInterval(function () {
         if (timeLeft > 1) {
-          timerEl.textContent = timeLeft + ' seconds remaining';
+          timerEl.textContent = timeLeft + " minutes remaining";
           timeLeft--;
         } else if (timeLeft === 1) {
-          timerEl.textContent = timeLeft + ' second remaining';
+          timerEl.textContent = timeLeft + " minutes remaining";
           timeLeft--;
         } else {
-          timerEl.textContent = '';
+          timerEl.textContent = "Your time is up.";
           clearInterval(timeInterval);
     }
     }, 1000);
 }
 
 
-countdown();
-quizStarted ();
