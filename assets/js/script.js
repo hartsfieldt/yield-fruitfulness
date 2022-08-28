78445d27285e1e812cc6abb65cf8daf3dfb91e73
Your Name <you@example.com>
2022-08-28 16:00:31 -0400

    updated questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,61 @@
-var questionIndex = 0;
-
-var timerEl = document.getElementById("timer");
-var questionHeader = document.getElementById("question-header");
+var questions = document.getElementById("questions");
 var mainPage = document.getElementById("main-page");
 var quizPage = document.getElementById("quiz-page");
-var quizButtons = document.querySelector(".answers");// everything above is correct //
 
-var timeLeft = 100;
+highScoreArray = [];
+letShuffledQuestions = []
+var questionCount = 0;
+var answersCorrect = 0;
+let countdown = 75;
 
-var quizList = [
+var timerText = document.getElementById("timer");
+timerText.textContent = "Timer: " + countdown;
+
+var quizButtons = document.getElementById(".answers");
+var answerButtonOne = document.getElementById("#answer-one");
+var answerButtonTwo = document.getElementById("#answer-two");
+var answerButtonThree = document.getElementById("#answer-three");
+var answerButtonFour = document.getElementById("#answer-four");
+
+const questionList = [
     {
         question: "Javascript is an _______ language?",
-        choices: ["object-oriented", "objective-base", "\n", "procedural", "\n", "none of the above"],
-        answer: "object-oriented"
+        answerOne: "object-oriented",
+        answerTwo:"objective-base",
+        answerThree:"procedural",
+        answerFour:"none of the above",
+        correctAnswer: "answerA",
     },
     {
         question: "Which of the following keywords is used to define a variable in Javascript?",
-        choices: ["var", "let", "Both A and B", "none of the above"],
-        answer: "var"
+        answerOne: "var",
+        answerTwo:"let",
+        answerThree:"Both A and B",
+        answerFour:"none of the above",
+        correctAnswer: "answerA",
     },
     {
         question: "Which of the following methods is used to access HTML elements using Javascript?",
-        choices: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "none of the above"],
-        answer: "Both A and B"
+        answerOne: "getElementbyId()",
+        answerTwo:"getElementsByClassName()",
+        answerThree:"Both A and B",
+        answerFour:"none of the above",
+        correctAnswer: "answerThree",
     },
     {
         question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
-        choices: ["Throws an error", "Ignores the statements", "Gives a warning", "none of the above"],
-        answer: "Ignores the statements"
+        answerOne: "Throws an error",
+        answerTwo:"Ignores the statements",
+        answerThree:"Gives a warning",
+        answerFour:"none of the above",
+        correctAnswer: "AnswerTwo",
     },
     {   question: "Which of the following methods can be used to display data in some form using Javascript?",
-        choices: ["document.write()?", "console.log()", "window.alert()", "All of the above"],
-        answer: "All of the above"
+        answerOne: "document.write()?",
+        answerTwo:"console.log()",
+        answerThree:"window.alert()",
+        answerFour:"All of the above",
+        correctAnswer: "answerFour",
     }
 ]
 
@@ -41,43 +65,30 @@ startButton.addEventListener("click", quizStarted);
 function quizStarted () {
     mainPage.classList.add("hide");
     quizPage.classList.remove("hide");
+    beginCountdown();
     generateQuestions();
 }
 
 function generateQuestions () {
-    var currentQuestion  = quizList[questionIndex];
-    questionIndex.textContent = currentQuestion.question;
-    choiceContainer.innerHTML =  "";
 
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
-    var choiceOptions = currentQuestion.choices[i];
-    var choiceElement = document.createElement("button");
-    choiceElement.setAttribute("class", "btn");
-    choiceElement.setAttribute("value", "choice");
-}
+    document.getElementById("#questions").innerHTML = currentQuestion.question;
+    document.getElementById("#answer-one").innerHTML = currentQuestion.answerOne;
+    document.getElementById("#answer-two").innerHTML = currentQuestion.answerTwo;
+    document.getElementById("#answer-three").innerHTML = currentQuestion.answerThree;
+    document.getElementById("#answer-four").innerHTML = currentQuestion.answerFour;
+
+    answerButtons.addEventListener("click", nextQuestion);
 }
 
-function nextQuestion (event) {
-    var answerButton = event.target;
-}
-
-var nextQuestion = function() {
-    questionIndex++;
-}
-
-
-var timeInterval = setInterval (function() {
-    if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + " seconds remaining";
-        timeLeft--;
-        } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + " seconds remaining";
-        timeLeft--;
+function beginCountdown() {
+    var timeInterval = setInterval(function () {
+        if (countdown >= 1) {
+          timerText.textContent = "Timer: " + countdown + " minutes remaining";
+          countdown--;
         } else {
-          timerEl.textContent = "Your time is up.";
+          timerText.textContent = "Your time is up.";
           clearInterval(timeInterval);
+          finishedQuiz();
     }
-}, 1000);
-
-
-
+    }, 1000);
+}
